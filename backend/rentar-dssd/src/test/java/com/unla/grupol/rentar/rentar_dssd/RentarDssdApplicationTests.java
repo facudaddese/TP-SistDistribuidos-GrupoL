96f024e39394d92b803052ee1a4d9c5d18e8cf36
Alejo Brites <alejobrites456@gmail.com>
2026-09-10 22:05:00 -0300

    feat: Estructurado del backend

---
+++ backend/rentar-dssd/src/test/java/com/unla/grupol/rentar/rentar_dssd/RentarDssdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupol.rentar.rentar_dssd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentarDssdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
