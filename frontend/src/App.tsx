248dfe3ac9ccbfa8d43ea4d09769c1c3fb033ece
Marco Carreira <marco.carreira.22n@gmail.com>
2026-09-18 00:52:16 -0300

    feat: crear logica para el abm de vehiculos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import home from "./assets/img/home.jpg";
 import { Route, Routes } from "react-router-dom";
 import Reservar from "./components/reservar/Reservar";
 import Cliente from "./components/cliente/Cliente";
+import AdminVehiculos from "./components/admin/AdminVehiculos";
 import HistorialAlquileres from "./components/historial-alquileres/HistorialAlquileres";
 import MisReservas from "./components/mis-reservas/MisReservas";
 import ConsultarDisponibilidad from "./components/consultar-disponibilidad/ConsultarDisponibilidad";
@@ -24,7 +25,7 @@ function App() {
           path="/consultar-disponibilidad"
           element={<ConsultarDisponibilidad />}
         />
-        {/* <Route path="/admin/vehiculos" element={<AdminVehiculos />} /> */}
+        <Route path="/admin/vehiculos" element={<AdminVehiculos />} />
         <Route path="/" element={<MainLayout />} />
         <Route path="*" element={<h4>404</h4>} />
       </Routes>
